da6b195df588a6e273720c277a9e91df96b9c886
Brandon <25nguyenb26@stu.smuhsd.org>
2024-01-19 15:37:50 -0800

    new comments

--- Robot.java
+++ Robot.java
@@ -1,3 +1,10 @@
 public class Robot {
-    
+    public static void periodic(){
+        // Place all the repeated code here;
+
+        // Check ultrasound
+        // Update position
+        // Run new move command (direction)
+        
+    }
 }
